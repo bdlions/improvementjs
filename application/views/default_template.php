e2896ab104dad522ce22b1a305b80a77fc20a57e
Alamgir Kabir <alamgirkabir7@gmail.com>
2014-12-23 16:26:55 +0600

    reference problem

--- application/views/default_template.php
+++ application/views/default_template.php
@@ -23,10 +23,11 @@
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/jquery-2.1.3.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/jquery-ui.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/jquery-ui.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/bootstrap-hover-dropdown.js"></script>
+        
         
         <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/beautify.js"></script>
         <script type="text/javascript" src="<?php echo base_url(); ?>resources/js/custom_script.js"></script>
